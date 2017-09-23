d497a072ac162da02d057e90c215ba0d62d75095
UZAIR MAJEED <uzairmajeed33@outlook.com>
2017-09-23 09:03:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/uzair/toast_pushnotifictions/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.uzair.toast_pushnotifictions;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+}
